a1102bcf0c2b2508186dbc80e9e64e4e72199f3a
Jlarragamena <jlarragamena@trueblue.com>
2024-06-25 11:24:04 -0500

    Exploring features of Spring Data JPA

--- src/main/java/ec/clicka/springboot/learnjpaandhibernate/course/springdatajpa/CourseSpringDataJpaRepository.java
+++ src/main/java/ec/clicka/springboot/learnjpaandhibernate/course/springdatajpa/CourseSpringDataJpaRepository.java
@@ -1,12 +1,13 @@
 package ec.clicka.springboot.learnjpaandhibernate.course.springdatajpa;
 
 import ec.clicka.springboot.learnjpaandhibernate.course.Course;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 public interface CourseSpringDataJpaRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findByAuthor(String author);
+    List<Course> findByName(String name);
 
 }

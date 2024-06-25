4040801afd48a916ca29658bd81152b89911959f
Jlarragamena <jlarragamena@trueblue.com>
2024-06-25 10:33:19 -0500

    Querying data using Spring JDBC

--- src/main/java/ec/clicka/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
+++ src/main/java/ec/clicka/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -2,6 +2,7 @@ package ec.clicka.springboot.learnjpaandhibernate.course.jdbc;
 
 import ec.clicka.springboot.learnjpaandhibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -22,6 +23,11 @@ public class CourseJdbcRepository {
                 delete from course where id = ?;
             """;
 
+    private static String SELECT_QUERY =
+            """
+                select * from course where id = ?;
+            """;
+
     public void insert(Course course) {
         springJdbcTemplate.update(INSERT_QUERY,
                 course.getId(),
@@ -31,6 +37,13 @@ public class CourseJdbcRepository {
 
     public void deleteById(long id) {
         springJdbcTemplate.update(DELETE_QUERY,id);
+    }
+
+    public Course findById(long id) {
+        // ResultSet -> Bean => Row Mapper
+        return springJdbcTemplate.queryForObject(SELECT_QUERY,
+                new BeanPropertyRowMapper<>(Course.class),id);
+
     }
 
 }

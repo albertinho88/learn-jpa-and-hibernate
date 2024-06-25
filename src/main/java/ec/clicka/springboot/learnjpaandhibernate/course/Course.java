780a886403a77f70fbec224e9f2abc4b0787aa5c
Jlarragamena <jlarragamena@trueblue.com>
2024-06-25 11:03:00 -0500

    Getting started with JPA and EntityManger

--- src/main/java/ec/clicka/springboot/learnjpaandhibernate/course/Course.java
+++ src/main/java/ec/clicka/springboot/learnjpaandhibernate/course/Course.java
@@ -1,5 +1,8 @@
 package ec.clicka.springboot.learnjpaandhibernate.course;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +12,12 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 @NoArgsConstructor
+@Entity
 public class Course {
+
+    @Id
     private long id;
     private String name;
     private String author;
+
 }
